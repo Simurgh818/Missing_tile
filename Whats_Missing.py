99502bc6f60bfbeafc01a82dc49d2c47b46cd11e
Simurgh818 <sina.dabiri@berkeley.edu>
2019-02-06 17:29:10 -0800

    added number of channel recognition from csv file.

--- Whats_Missing.py
+++ Whats_Missing.py
@@ -4,25 +4,31 @@ import csv
 
 csv_file = []
 csv_file_tile = []
+csv_file_channel = []
 template_range = 0
 
-with open('/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA2-1-DR/JAK_MPA2_1_DR.csv', newline='') as f:
-	reader = csv.reader(f)
+with open('/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA2-1-DR/JAK_MPA2_1_DR.csv', newline='\n') as f:
+	reader = csv.reader(f, delimiter=',')
+# , delimiter=';'
+	# print(reader.size)
+
 	for row in reader:
-		# print(row)
+		# row = row[0]
+		# print(row[0])
 		csv_file[len(csv_file):] = [row[0]]
 		csv_file_tile[len(csv_file_tile):]= [row[2]]
+		csv_file_channel[len(csv_file_channel):]= [row[4]]
 
 print(len(csv_file))
-# print(csv_file,'\n')
+
 if 'Well' in csv_file:
 	well_index = csv_file.index('Well')+1
 	# print("the well element is on: ",well_index)
-	print("the wells are: ", csv_file[well_index:],'\n')
+	# print("the wells are: ", csv_file[well_index:],'\n')
 	well_plate = len(csv_file[well_index:])
 	print("We got ", well_plate, "wells in the plate.",'\n')
 
-print(csv_file_tile,'\n')
+# print(csv_file_tile,'\n')
 if 'Timepoint' in csv_file_tile:
 	timepoint_index = csv_file_tile.index('Timepoint')+1
 	timepoints = csv_file_tile[timepoint_index:csv_file_tile.index('Array')-1]
@@ -34,22 +40,26 @@ if 'Array' in csv_file_tile:
 	template_range = int(csv_file_tile[tile_index])**2
 	print("The number of tiles are ", template_range, '\n')
 
+if 'Channels' in csv_file_channel:
+	channels_index = csv_file_channel.index('Channels')+1
+	channels = csv_file_channel[channels_index]
+	channels_array = channels.split(';')
+	number_of_channels = len(channels_array)
+	print("number of channels are ", number_of_channels, '\n')
 
-# well_plate = 96
-#  input("How many well plate did you use? Type 72, 96,or 384? ")
+# print(csv_file_channel,'\n')
+
 if well_plate ==72:
 	plate_rows = ['A','B','C','D','E','F','G','H']
 	plate_columns = ['1','2','3','4','5','6','7','8','9']
 elif well_plate ==96:
 	plate_rows = ['A','B','C','D','E','F','G','H']
 	plate_columns = ['1','2','3','4','5','6','7','8','9','10','11','12']
+elif well_plate ==384:
+	plate_rows = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P']
+	plate_columns = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
 
-number_of_channels = 3
-# image_tiles = 4
-# input("What's the image template for this dataset? Press 2 for 2x2, 3 for 3x3, 4 for 4x4 and 5 for 5x5: ")
-# expected number of images is calculated by multiplying wells in a plate by number of tiles in a well and
-# 	adding one for the Fiduciary
-# template_range = image_tiles**2
+# number_of_channels = 3
 
 expected_number_of_images = well_plate*(template_range)*number_of_channels
 print("The expected number of images are: ", expected_number_of_images,'\n')
@@ -65,8 +75,8 @@ print("The wells are: ",well, '\n')
 plate_dir = '/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA2-1-DR/'
 for lf in os.listdir(plate_dir):
 	print(lf)
-	if lf.endswith('.log') and lf.find('JAK-MPA2-1-DR-T10.log')>=0:
-		print('\n',lf)
+	if lf.endswith('.log') and lf.find('JAK-MPA2-1-DR-T7.log')>=0:
+		# print('\n',lf)
 		well_with_missing_tile = []
 		current_line = []
 
