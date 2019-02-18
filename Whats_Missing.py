0d36fa068653a1d25b2ff819e20ad7441308982a
Simurgh818 <sina.dabiri@berkeley.edu>
2019-02-18 11:20:39 -0800

    T17 bug was fixed.

--- Whats_Missing.py
+++ Whats_Missing.py
@@ -6,20 +6,24 @@ csv_file = []
 csv_file_tile = []
 csv_file_channel = []
 template_range = 0
+well_plate = 0
 
-with open('/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA2-1-DR/JAK_MPA2_1_DR.csv', newline='\n') as f:
+with open('/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA1-TL-2/JAK-MPA1-TL-2-RowAB/JAK-MPA1-TL-2-RowAB.csv', \
+	newline='\n') as f:
 	reader = csv.reader(f, delimiter=',')
 # , delimiter=';'
-	# print(reader.size)
+	# print(next(reader))
 
 	for row in reader:
 		# row = row[0]
-		# print(row[0])
-		csv_file[len(csv_file):] = [row[0]]
-		csv_file_tile[len(csv_file_tile):]= [row[2]]
-		csv_file_channel[len(csv_file_channel):]= [row[4]]
+		# next(reader)
+		# print("length of row is: ",len(row),"tile", row[4:5])
 
-print(len(csv_file))
+		csv_file[len(csv_file):] = row[0:1]
+		csv_file_tile[len(csv_file_tile):]= row[2:3]
+		csv_file_channel[len(csv_file_channel):]= row[4:5]
+
+# print(csv_file,'\n\n',csv_file_tile,'\n\n',csv_file_channel)
 
 if 'Well' in csv_file:
 	well_index = csv_file.index('Well')+1
@@ -31,7 +35,7 @@ if 'Well' in csv_file:
 # print(csv_file_tile,'\n')
 if 'Timepoint' in csv_file_tile:
 	timepoint_index = csv_file_tile.index('Timepoint')+1
-	timepoints = csv_file_tile[timepoint_index:csv_file_tile.index('Array')-1]
+	timepoints = csv_file_tile[timepoint_index:csv_file_tile.index('Array')]
 	print("The timepoints are: ", timepoints)
 
 if 'Array' in csv_file_tile:
@@ -44,43 +48,31 @@ if 'Channels' in csv_file_channel:
 	channels_index = csv_file_channel.index('Channels')+1
 	channels = csv_file_channel[channels_index]
 	channels_array = channels.split(';')
+	print(channels_array)
 	number_of_channels = len(channels_array)
 	print("number of channels are ", number_of_channels, '\n')
 
 # print(csv_file_channel,'\n')
 
-if well_plate ==72:
-	plate_rows = ['A','B','C','D','E','F','G','H']
-	plate_columns = ['1','2','3','4','5','6','7','8','9']
-elif well_plate ==96:
-	plate_rows = ['A','B','C','D','E','F','G','H']
-	plate_columns = ['1','2','3','4','5','6','7','8','9','10','11','12']
-elif well_plate ==384:
-	plate_rows = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P']
-	plate_columns = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
+# well = []
 
-# number_of_channels = 3
-
-expected_number_of_images = well_plate*(template_range)*number_of_channels
-print("The expected number of images are: ", expected_number_of_images,'\n')
-
-well = []
-
-
-for w_alpha in range(0,len(plate_rows)):
-	for w_num in range(0, len(plate_columns)):
-		well.append(plate_rows[w_alpha]+plate_columns[w_num])
-print("The wells are: ",well, '\n')
-
-plate_dir = '/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA2-1-DR/'
+plate_dir = '/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-MPA1-TL-2/JAK-MPA1-TL-2-RowAB/'
 for lf in os.listdir(plate_dir):
 	print(lf)
-	if lf.endswith('.log') and lf.find('JAK-MPA2-1-DR-T7.log')>=0:
-		# print('\n',lf)
-		well_with_missing_tile = []
-		current_line = []
+	if lf.endswith('.log') and lf.find('JAK-MPA1-TL-2-RowAB-T17.log')>=0:
+		# and lf.find('JAK-MPA1-TL-2-RowAB-T8.log')>=0
+		if lf.find('T0')>=0:
+			expected_number_of_images = well_plate*(template_range)*number_of_channels
+			print("The expected number of images are: ", expected_number_of_images,'\n')
+		else:
+			expected_number_of_images = well_plate*(template_range)
+			print("The expected number of images are: ", expected_number_of_images,'\n')
+			channels_array = channels_array[1:]
 
-		path = plate_dir + lf
+		# print('\n',lf)
+
+
+		path = os.path.join(plate_dir,lf)
 		# path = '/mnt/finkbeinernas/robodata/Sina/Robo3_Images/JAK-JEL22-Plate2/JAK-JEL22-Plate2-T0_1.log'
 		# input("What's the path location of the log file you like to open? ")
 
@@ -88,54 +80,85 @@ for lf in os.listdir(plate_dir):
 		print("Path is: ",path,'\n')
 		log_file = open(path, 'r')
 		temp = log_file.read()
-		split_log_file = temp.split(' ; \n')
-		# print(split_log_file)
+		split_log_file = temp.split('\n\n')
+		# (' ; \n')
+		# ### The split log file is:
+		# print("The Split log file is: ", split_log_file)
 		# if split_log_file.find('FIDUCIARY')>0:
-		length_split_log_file = len(split_log_file)
+		length_split_log_file = len(split_log_file)-1
 
 		print("log file length is: ", length_split_log_file)
-		for file in range(1, len(split_log_file)-1):
-			if split_log_file[file].find('FIDUCIARY')>=0 or split_log_file[file].find('ROTFID')>=0:
+		for file in range(0, len(split_log_file)):
+			if split_log_file[file].find('FIDUCIARY_MONTAGE')>=0 or \
+				split_log_file[file].find('ROTFID_STACK')>=0 or \
+				split_log_file[file].find('ROTFID_MONTAGE')>=0:
+
 				length_split_log_file = length_split_log_file-1
-
-
+				# print("found one at ", file, length_split_log_file)
+		well_with_missing_tile = []
+		current_line = [' ']*len(split_log_file)
+		# print("current_line: ", len(current_line), '\n')
+		print("log file length after subtracting FIDUCIARY and ROTFID is: ", length_split_log_file,'\n')
 		# print(split_log_file)
 		file =1
 		count_img = 0
-		while file<(len(split_log_file)):
+		count = 0
+		while file<len(split_log_file):
 			# print(file)
-			# if split_log_file[file].find('FIDUCIARY')>0:
-			# 	length_split_log_file = length_split_log_file-1
-			if split_log_file[file].find('FIDUCIARY')<=0 and split_log_file[file].find('ROTFID')<=0:
+
+			if split_log_file[file].find('FIDUCIARY')<=0 or split_log_file[file].find('ROTFID')<=0 \
+				or split_log_file[file].find('\n')<=0:
 
 				# loop through all expected wells
-				for w in well:
-					# print(well)
+				for w in csv_file[well_index:]:
+					# print(w)
 					count =0
 					for template in range(1,template_range+1):
-						if file < length_split_log_file:
-
-							for cl in range (1,len(split_log_file)):
-								if split_log_file[cl].split('_')[4].find(str(w))>=0 and split_log_file[cl].split('_')[5].find(str(template))>=0:
-									current_line = split_log_file[cl]
-									time_stamp = split_log_file[cl].split('--')[0]
+					# loop through expected tiles
+						for ch in channels_array:
+							# loop through expected channels
+							# print("we are on channel: ", ch)
+							time_stamp = [' ']*len(split_log_file)
+							for cl in range (0,len(split_log_file)-1):
+								# check the whole log for the specific file entry
+								current_well = str(split_log_file[cl].split('_')[4])
+								current_tile = str(split_log_file[cl].split('_')[5])
+								current_channel = str(split_log_file[cl].split('_')[6].split('.')[0])
+								time_stamp[cl] = split_log_file[cl].split(' ')[3]
+								# print("current_well is: ", current_well, "current_tile is: ", current_tile \
+									# , "current_channel is :", current_channel)
+								# print(current_well == str(w), current_tile == str(template),\
+										# current_channel == str(ch))
+								# split_log_file[cl].find('.tif')>=0  and \
+								if (current_well == str(w) and \
+										current_tile == str(template) and \
+										current_channel == str(ch)):
+									# print(split_log_file[cl])
+									current_line[cl] = split_log_file[cl]
 									count_img = count_img+1
+									# print(file, w,', panel ', template, ch, ': ',current_line[cl])
 									file = file+1
+									# cl=0
 									break
 								else:
-									current_line = ''
+									# print("cl is: ", cl)
+									current_line[cl] = ' '
 
 
-							# print(w,', panel ', template, ': ',current_line)
-							if current_line == '':
+							## print(w,', panel ', template, ': ',current_line[cl])
+							# instead do a loop to check all lines for blank.
+							if current_line[cl] == ' ':
 								count = count+1
-								well_with_missing_tile[len(well_with_missing_tile):] = [(w, template, time_stamp)]
+								well_with_missing_tile[len(well_with_missing_tile):] = \
+								[(w, template, ch, time_stamp[cl])]
+								# print(file, w,', panel ', template, ch, ': ',current_line[cl])
 
-						else:
+
+						if file > expected_number_of_images or file > length_split_log_file:
 							break
 					# print('------------------------------------------------------------------')
-				if file >= length_split_log_file:
-					break
+					if file > expected_number_of_images or file > length_split_log_file:
+						break
 
 			else:
 				file = file+1
@@ -151,32 +174,5 @@ for lf in os.listdir(plate_dir):
 			if i ==0:
 				print('-'*len(line))
 			pass
-		print('------------------------------------------------------------------')
-
-# for file in range(0,len(split_log_file)-1):
-# 	file_entry = split_log_file[file]
-# 	temp2 = file_entry.split(' -- ')
-# 	file_name = temp2[1]
-# 	if file_name.find('FIDUCIARY')<=0:
-# 		count =0
-# 		for template in range(1,template_range+1):
-# 			# position = file+template
-# 			if file < len(split_log_file)-1:
-# 				current_line = split_log_file[template]
-# 				if len(current_line.split('_'))>=2:
-# 					well = current_line.split('_')[4]
-# 					tile = current_line.split('_')[5]
-# 					# print("we are on tile: ", tile)
-# 					if  tile != template:
-# 						np.add(count,1)
-# 						print(well,', panel ', template, ':')
-# 					else:
-# 						print(well,', panel ', template, ': ',current_line)
-# 			else:
-# 				break
-# 			# print(well,', panel ', template, ': ',current_line)
-# 		file = file + template
-
-# 	print('------------------------------------------------------------------')
-
+		print('------------------------------------------------------------------------------------------------')
 
